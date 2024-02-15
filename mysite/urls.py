8cae095c0543971cbbcc98572e1ee26817aca8c3
Clinton Nwachukwu <Clintonmatics@gmail.com>
2024-02-15 17:45:03 +0100

    adjusted url conf

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('addd/', admin.site.urls),
-    path("blog", include("blogapp.urls"))
+    path("blog/", include("blogapp.urls"))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
